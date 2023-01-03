af66a86f394ab036a3c4472790cfadadeb578b78
cathy <cathy@bubulazi.com>
2023-01-03 16:12:02 +0000

    Colocados todos los botones

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import '../hojas-de-estilo/Boton.css'
 
 export default function Boton(props) {
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=')
+  }
+
   return(
-    <div className={`boton-contenedor ${}`}>
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : '' }`.trimEnd()}>
       {props.children}
     </div>
   )
