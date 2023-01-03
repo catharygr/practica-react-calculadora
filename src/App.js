003324c4cde52bdb9ab11038cd51ff9f28d19fe6
cathy <cathy@bubulazi.com>
2023-01-03 18:13:55 +0000

    Trabajando con useState

--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
-import React from 'react'
 import './App.css';
-import freecodecamp from './imagenes/freecodecamp-logo.png';
-import Boton from './componentes/Boton'
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("Por favor ingrese valores para realizar los cálculos.");
+    }
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
         <img 
-          src={freecodecamp}
+          src={freeCodeCampLogo}
           className='freecodecamp-logo'
-          alt='Logo de freecodecamp'
-        />
+          alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-calculadora'>
+        <Pantalla input={input}/>
         <div className='fila'>
-        <Boton>1</Boton>
-        <Boton>2</Boton>
-        <Boton>3</Boton>
-        <Boton>+</Boton>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
         </div>
         <div className='fila'>
-        <Boton>4</Boton>
-        <Boton>5</Boton>
-        <Boton>6</Boton>
-        <Boton>-</Boton>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
         </div>
         <div className='fila'>
-        <Boton>7</Boton>
-        <Boton>8</Boton>
-        <Boton>9</Boton>
-        <Boton>*</Boton>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
         </div>
         <div className='fila'>
-        <Boton>=</Boton>
-        <Boton>0</Boton>
-        <Boton>.</Boton>
-        <Boton>/</Boton>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
         </div>
-        <div className='fila'></div>
-        <div></div>
-
-
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>
+            Clear
+          </BotonClear>
+        </div>
       </div>
-   
     </div>
   );
 }
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import '../hojas-de-estilo/Boton.css'
+import React from 'react';
+import '../hojas-de-estilo/Boton.css';
 
 export default function Boton(props) {
-  const esOperador = valor => {
-    return isNaN(valor) && (valor !== '.') && (valor !== '=')
-  }
 
-  return(
-    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : '' }`.trimEnd()}>
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !=='.') && (valor !== '=');
+  };
+
+  return (
+    <div
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+      onClick={() => props.manejarClic(props.children)}>
       {props.children}
     </div>
-  )
+  );
 }
