f056cc502ff1ca7ae455eb5c10e7ece55cfe5e40
cathy <cathy@bubulazi.com>
2023-01-04 11:50:18 +0000

    Haciendo algunos arreglos

--- src/componentes/BotonClear.js
+++ src/componentes/BotonClear.js
@@ -2,8 +2,9 @@ import React from 'react';
 import '../hojas-de-estilo/BotonClear.css';
 
 const BotonClear = (props) => (
-  <div className='boton-clear' onClick={props.manejarClear}>
-    {props.children}
+  <div className='boton-clear' 
+       onClick={props.manejarClear}>
+        {props.children}
   </div>
 );
 
